ea0366733c546178ad812e93e34cc5b8bd6c3355
Timothy Ragan <timothyragan7@gmail.com>
2019-07-05 22:51:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -32,7 +32,7 @@ def toadstool
  status="Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
-def any_phrase
+def any_phrase(phrase)
  any_phrase="Do A Barrel Roll!"
  puts any_phrase
  end
