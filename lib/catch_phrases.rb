d19973217fe1e4e78771378c1942bcc5db1a9e57
Timothy Ragan <timothyragan7@gmail.com>
2019-07-05 22:56:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,23 +1,18 @@
-
-
-
 def mario
-  
-   phrase="It's-a me, Mario!" 
+  phrase="It's-a me, Mario!" 
   puts phrase
- 
 end
 
 def toadstool
- status="Thank You Mario! But Our Princess Is In Another Castle!"
+  status="Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
-def any_phrase(phrase)
- any_phrase="Do A Barrel Roll!"
- puts any_phrase
- end
- 
 
+def any_phrase(phrase)
+  any_phrase="Do A Barrel Roll!"
+  puts any_phrase
+end
+ 
 def link
   link="It's Dangerous To Go Alone! Take This."
   puts link
